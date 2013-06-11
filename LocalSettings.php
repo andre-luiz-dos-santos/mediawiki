184d8fd266cfbbddb8328b89f4368b1994db9f36
Jared Szechy <jared.szechy@gmail.com>
2013-06-11 01:10:24 -0400

    Verbose url

--- LocalSettings.php
+++ LocalSettings.php
@@ -61,6 +61,7 @@ if (preg_match('%(.*?)://([^:]+):([^@]+)@([^:]+):(\d+)/(.*)%', $_wgDBConnectionS
     $wgDBport = $regs[5][0];
     $wgDBname = $regs[6][0];
 } else {
+    print_r($_wgDBConnectionString);
     print_r(array_values($regs));
     die("Failed to parse DB connection string");
 }
