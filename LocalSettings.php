9bc9cc879addbe7b55de49694322255caba74813
Jared Szechy <jared.szechy@gmail.com>
2013-06-19 00:12:36 -0400

    Update fqdn

--- LocalSettings.php
+++ LocalSettings.php
@@ -30,7 +30,7 @@ $wgScriptPath = "";
 $wgScriptExtension = ".php";
 
 ## The protocol and server name to use in fully-qualified URLs
-$wgServer = "http://erauwiki.herokuapp.com";
+$wgServer = "http://wiki.eraumotorsports.com";
 
 ## The relative URL path to the skins directory
 $wgStylePath = "$wgScriptPath/skins";
